95be12b66caac63afbe025eefa58c53bd8a85101
Alexander Telyatnikov <labriko@yandex.ru>
2017-01-02 09:35:55 +0300

    07 webpack client app

---
+++ src/client/app.js
@@ -0,0 +1,8 @@
+import 'babel-polyfill';
+
+import Dog from '../shared/dog';
+
+const browserToby = new Dog('Browser Toby');
+
+document.querySelector('.app').innerText = browserToby.bark(); // eslint-disable-line
+
